b8de37d2c38aab295a40bd2d73439ceade9abbb4
Vmer <->
2018-05-30 00:13:12 -0700

    Test for CI

--- elastic-search-engine/Startup.cs
+++ elastic-search-engine/Startup.cs
@@ -27,7 +27,7 @@ namespace elastic_search_engine
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("CI Test!");
             });
         }
     }
